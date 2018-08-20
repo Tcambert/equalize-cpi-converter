a7c50b567762b79bfaf87129683d29012f8cc4ed
engswee <engswee+git@gmail.com>
2018-08-20 11:29:35 +0800

    Backup of simple factory implementation for ConverterFactory

--- converter-core/src/main/groovy/com/equalize/cpi/converter/util/ConverterFactory.groovy
+++ converter-core/src/main/groovy/com/equalize/cpi/converter/util/ConverterFactory.groovy
@@ -1,7 +1,11 @@
 package com.equalize.cpi.converter.util
 
 import org.apache.camel.Exchange
-import java.lang.reflect.Constructor
+
+import com.equalize.cpi.converter.Base64DecodeConverter
+import com.equalize.cpi.converter.Base64EncodeConverter
+import com.equalize.cpi.converter.JSON2XMLConverter
+import com.equalize.cpi.converter.XML2JSONConverter
 
 class ConverterFactory {
 	// Private constructor to prevent direct instantiation of this factory
@@ -15,13 +19,18 @@ class ConverterFactory {
 	AbstractConverter newConverter(Exchange exchange, Map<String,Object> properties) {
 		PropertyHelper ph = new PropertyHelper(properties)
 		String converterClassName = ph.getProperty('converterClass')
-		try {
-			// Dynamic loading and instantiation of converter class
-			Class<?> converterClass = Class.forName(converterClassName)
-			Constructor<?> constructor = converterClass.getConstructor(Exchange, Map);
-			return (AbstractConverter) constructor.newInstance(exchange, properties)
-		} catch(ClassNotFoundException e) {
-			throw new ClassNotFoundException("$converterClassName is an invalid converter class")
+
+		switch(converterClassName) {
+			case 'com.equalize.cpi.converter.Base64DecodeConverter':
+				return new Base64DecodeConverter(exchange, properties)
+			case 'com.equalize.cpi.converter.Base64EncodeConverter':
+				return new Base64EncodeConverter(exchange, properties)
+			case 'com.equalize.cpi.converter.JSON2XMLConverter':
+				return new JSON2XMLConverter(exchange, properties)
+			case 'com.equalize.cpi.converter.XML2JSONConverter':
+				return new XML2JSONConverter(exchange, properties)
+			default:
+				throw new ClassNotFoundException("$converterClassName is an invalid converter class")
 		}
 	}
 }
